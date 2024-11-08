0580127f716ca327ee3dc63a00547c6ebee2ced3
Eli <elimbukoski@gmail.com>
2024-11-07 19:03:48 -0500

    update bio page
    Former-commit-id: d89aad95b329fa49a50f9ee3ba13848300ff5d0d

--- src/routes/eli/+page.svelte
+++ src/routes/eli/+page.svelte
@@ -19,10 +19,57 @@
 		<h2>Eli Bukoski</h2>
 		<div class="bar" />
 		<p>Electrical Engineer</p>
-		{#each bioData.locations as location}
-			<p>{location}</p>
-		{/each}
 		<div class="spacer" style="height: 1em;"></div>
+		<p>
+			{bioData.locations.join(' - ')}
+		</p>
 	</div>
-	<div class="two-thirds"></div>
+	<div class="two-thirds">
+		<h2>Work Experience</h2>
+
+		{#if bioData.skills}
+			<h2>Skills</h2>
+			<div class="flex-container" style="display:flex; flex-wrap: wrap;">
+				<div class="vertical-bar" />
+				{#each bioData.skills as skill}
+					<div class="column">
+						<h3>{skill.category}</h3>
+						{#each skill.skills as skillText}
+							<p>{skillText}</p>
+						{/each}
+					</div>
+					<div class="vertical-bar" />
+				{/each}
+			</div>
+		{/if}
+		<h2>Education</h2>
+	</div>
 </div>
+
+<style>
+	.bar {
+		background-color: var(--theme-mid);
+	}
+	.thirds:hover .bar {
+		background-color: var(--theme-accent);
+	}
+
+	.vertical-bar {
+		width: 0.1em;
+		background-color: var(--theme-mid);
+		margin: 1em;
+		margin-top: 2em;
+		margin-bottom: 2em;
+		border-radius: 0.05em;
+		transition: all var(--transition-length);
+	}
+
+	.flex-container:hover .vertical-bar {
+		background-color: var(--theme-accent);
+	}
+
+	.column {
+		flex: 1;
+		text-wrap: wrap;
+	}
+</style>
