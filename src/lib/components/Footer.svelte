f8eac8315fe31002cfe14ade5029649ee3ee9fca
Mapy542 <elimbukoski@gmail.com>
2024-11-15 21:58:47 -0500

    update header

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import './radialBG.css';
+</script>
+
+<footer class="gradientBG-topFade">
+	<div class="footer-container">
+		<div class="footer-logo">
+			<a href="/">
+				<img src="/img/logo.webp" alt="logo" />
+			</a>
+		</div>
+		<div class="footer-links">
+			<a href="/about">About</a>
+			<a href="/contact">Contact</a>
+			<a href="/privacy">Privacy</a>
+		</div>
+	</div>
+</footer>
