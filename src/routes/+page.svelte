207a88b94fc2d4b36d53594b889af5806f833c5d
Mapy542 <elimbukoski@gmail.com>
2025-01-30 23:57:42 -0500

    update homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import DynaImage from '$lib/components/DynaImage.svelte';
+	import Pcb from '$lib/components/pcb.svelte';
 
 	/** @type {import('./$types').PageData} */
 	export let data;
@@ -15,13 +16,22 @@
 	<meta name="author" content="Eli Bukoski" />
 </svelte:head>
 
-<h1>Bukoski.dev</h1>
-<p>
-	Home of engineering project blogs and documentation. Here, you'll find projects that reflect my
-	passion for clean, efficient designs and smart problem-solving. I love taking on complex
-	challenges and finding elegant solutions, whether on a team or solo. Take a look to see how I
-	bring ideas to life!
-</p>
+<div class="overlap-hero">
+	<div class="pcb-container">
+		<Pcb />
+	</div>
+	<div class="hero-text">
+		<div style="width: 60vw;">
+			<h1>Bukoski.dev</h1>
+			<p>
+				Home of engineering project blogs and documentation. Here, you'll find projects that reflect
+				my passion for clean, efficient designs and smart problem-solving. I love taking on complex
+				challenges and finding elegant solutions, whether on a team or solo. Take a look to see how
+				I bring ideas to life!
+			</p>
+		</div>
+	</div>
+</div>
 
 <div class="flex-columns">
 	<div class="seconds">
@@ -53,5 +63,31 @@
 <style>
 	h1 {
 		width: 100%;
+	}
+
+	.overlap-hero {
+		width: 100%;
+		height: 90vh;
+	}
+
+	.hero-text {
+		position: relative;
+		top: -30%;
+		left: -10%;
+		width: 120%;
+		height: 30%; /* Cover the bottom 30% */
+		background-color: var(--theme-bg-primary);
+		background: linear-gradient(to bottom, RGBA(0, 0, 0, 0) 0%, var(--theme-bg-primary) 10%);
+		padding-top: 2em;
+		display: flex;
+		justify-content: center;
+	}
+
+	.pcb-container {
+		position: relative;
+		top: 0;
+		left: -10%;
+		width: 100%;
+		height: 100%; /* Ensure it covers the full height of the container */
 	}
 </style>
