eb1c3c4dab748748619eb83f00b589509f45d42b
Mapy542 <elimbukoski@gmail.com>
2025-02-04 11:32:23 -0500

    add content list
    Former-commit-id: e16e38cb27e706eaca7c8fe41fbd93e8e931f334

---
+++ src/routes/k400-home/+page.server.js
@@ -0,0 +1,29 @@
+import serverReader from "../../lib/components/serverReader.js";
+import categoryMetaData from "$lib/data/categoriesMeta.json" with {type: "json"};
+
+/**@type {import('./$types').PageLoad} */
+export async function load() {
+
+  let category="K400-Updates";
+    //otherwise, we can proceed assuming the category exists
+    let postMetas = {};
+    let postSrcs = [];
+try{
+     postMetas = serverReader.cachedStripMetaReplacement(Object.values(serverReader.quickIndex[category]) ?? []);
+     for(const src of Object.keys(postMetas)){
+            postSrcs.push(src.split("/data")[1]);
+        }
+} catch(e){
+    return {
+        status: 500,
+        error: e
+    }
+}
+    return {
+        props: {
+            "postMetas": Object.values(postMetas),
+            "postSrcs": postSrcs,
+        }
+    }
+
+}
---
+++ src/routes/k400-home/+page.svelte
@@ -0,0 +1,87 @@
+<script>
+	import DynaImage from '$lib/components/DynaImage.svelte';
+
+	export let data;
+</script>
+
+<div class="flex-columns">
+	<div class="thirds">
+		<h1 style="color:var(--theme-highlight);">K400 Laser Cutter</h1>
+
+		<div style="height: 1rem;" />
+
+		<p>
+			The K400 is my homemade large laser cutter based on the hobbyist generic CO2 laser, the K40.
+		</p>
+		<p>
+			These posts detail my design and build process along with some tips I learned that are not
+			common knowledge. Should you be interested in following the same path, there are design files
+			you can download and build upon!
+		</p>
+	</div>
+	<div class="two-thirds">
+		<DynaImage
+			src="laser-overview/far-oput-laser-in-laser.webp"
+			alt="K400 Laser Cutter"
+			scaleFactor=".66"
+			paddingCount="1"
+		/>
+	</div>
+</div>
+
+<div style="height: 3rem;" />
+
+<h2>Updates</h2>
+<div class="post-container">
+	{#if data.props.postMetas}
+		{#each data.props.postMetas as postMeta, index}
+			<a href={data.props.postSrcs[index].replace('.md', '')}>
+				<div class="flex-columns linkable">
+					{#if postMeta.image}
+						<div class="thirds inlinkable">
+							<DynaImage
+								src={postMeta.image}
+								alt={postMeta.title}
+								scaleFactor=".33"
+								paddingCount="1"
+							/>
+						</div>
+					{/if}
+					<div class="two-thirds inlinkable">
+						{#if postMeta.title}
+							<h2>{postMeta.title}</h2>
+						{/if}
+						<p>
+							{#if postMeta.author}{postMeta.author}{/if}
+							{#if postMeta.author && postMeta.date}
+								-
+							{/if}
+							{#if postMeta.date}{postMeta.date}{/if}
+						</p>
+						{#if postMeta.description}
+							<p>{postMeta.description}</p>
+						{/if}
+					</div>
+				</div>
+			</a>
+		{/each}
+	{/if}
+</div>
+
+<style>
+	.linkable {
+		border-radius: var(--theme-img-border-radius);
+		border-color: var(--theme-bg-primary);
+		border-style: solid;
+		margin-bottom: 1rem;
+		transition: all var(--transition-length);
+	}
+
+	.inlinkable {
+		border-radius: var(--theme-img-border-radius);
+	}
+
+	.linkable:hover {
+		border-color: var(--theme-link-hover);
+	}
+</style>
