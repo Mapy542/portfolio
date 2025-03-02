29ac849b23ed0e2088a35e88a97eb2c5888df73c
Eli <elimbukoski@gmail.com>
2025-03-01 22:29:52 -0500

    inroll xmldom

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { rollupVersion } from 'vite';
 
 
 export default {
-  plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  build:{
+    rollupOptions: {
+      internal: ['xmldom'] //i guess this forces the inclusion of xmldom
+    }
+  }
 }; 
