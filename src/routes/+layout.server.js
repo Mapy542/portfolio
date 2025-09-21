60fc204c6da382feddfed5629bcba44a5df8c61e
Eli <elimbukoski@gmail.com>
2025-09-21 13:55:47 -0400

    update prerendering and ssr rules sitewide
    Former-commit-id: 7b24eb5af554d7b1d73be9454c3d14708c784c7b

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,6 +1,10 @@
 import serverReader from '../lib/components/serverReader';
 import staticMetas from '$lib/data/staticMeta.json' with {type: "json"};
 
+export const csr = true; //header is client side rendered always
+export const prerender = 'auto'; //we want to prerender this page if possible
+export const ssr = true; //we want to server side render where possible i think
+
 /**@type {import('./$types').PageServerLoad} */
 export async function load() {
     let categories = serverReader.getCategories();
